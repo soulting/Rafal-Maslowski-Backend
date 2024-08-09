cd0417682090d1f300cf3676c46775bd2d34bbc5
soulting <andrzej_insadowski@onet.pl>
2024-08-09 20:05:58 +0200

    second commit for render

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello world")
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def handle_request():
+    if request.method == 'POST':
+        data = request.get_json()  # Odbiera dane w formacie JSON
+        print(f"Received POST request with data: {data}")
+        return jsonify({"status": "success", "data": data}), 200
+    elif request.method == 'GET':
+        print("Received GET request")
+        return jsonify({"status": "success", "message": "Hello, World!"}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
